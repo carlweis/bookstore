a192a50581b44f8d4b79fdc92294271694e3ab3d
Carl Weis <carl@carlweis.com>
2016-07-13 16:00:37 -0500

    Bug fixes

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
-    assert_select '#columns #side a', minimum: 4
+    assert_response :success    
     assert_select '#main .entry', 3
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
