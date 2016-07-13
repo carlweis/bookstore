cba41f67393090c216d547eca2633b60d53d25df
Carl Weis <carl@carlweis.com>
2016-07-13 02:34:11 -0500

    Update seeds.rb for title unique constraint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 require 'ffaker'
 
 # remove existing products
+LineItem.delete_all
 Product.delete_all
 
 # seed 100 random products
-100.times do
+100.times do |i|
   product_title = FFaker::Product.product
   Product.create!(
-    title: product_title,
+    title: "#{product_title}#{i}",
     description: FFaker::Lorem.paragraph(1),
     price: rand(1...100).to_f,
     image_url: "#{product_title.parameterize}.jpg"
